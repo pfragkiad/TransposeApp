afdff8757d038e765f0048fc0b83e252d54ccd91
Pavlos Fragkiadoulakis <pavfrang@office365.auth.gr>
2022-07-02 20:48:23 +0300

    Add WinTranspose project.

---
+++ WinTranspose/Form1.cs
@@ -0,0 +1,12 @@
+namespace WinTranspose
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("CREATED!!!");
+        }
+    }
+}
